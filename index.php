e17027a9bb54e4f061e08766a32808b57853d9b7
Student User <student@workstation.lab.example.com>
2022-01-06 18:03:26 -0500

    little change

--- index.php
+++ index.php
@@ -6,11 +6,9 @@
 
 <body> 
 	<h1>PHP Test</h1>
-		<p><b>An Example of PHP in Action</b></p>
+		<p><b> My New changed Example of PHP in Action</b></p>
 		<?php echo "The Current Date and Time is: <br />"; 
-		echo date("g:i A l, F j Y.");?> </p>
-	<h2>PHP Information</h2> 
-		<p> <?php phpinfo(); ?> </p> 
+		echo date("g:i A l, F j Y.");?> </p> 
         <h2> HELLO FOLKS</h2>
 	</body> 
 </html>
