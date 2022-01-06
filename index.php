cccfb3c1f4e4b2bfc7d32bed3acc0fc925127e39
Student User <student@workstation.lab.example.com>
2022-01-06 18:59:09 -0500

    anoother change

--- index.php
+++ index.php
@@ -1,14 +1,14 @@
 <html> 
 	<head> 
-	<title>PHP Test</title>
+	<title>PHP NEW Test</title>
 	<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1"/>
 	</head>
 
 <body> 
 	<h1>PHP Test</h1>
-		<p><b> My New changed Example of PHP in Action</b></p>
+		<p><b> My Second New changed Example of PHP in Action</b></p>
 		<?php echo "The Current Date and Time is: <br />"; 
 		echo date("g:i A l, F j Y.");?> </p> 
-        <h2> HELLO FOLKS</h2>
+        <h2> Hola amigo</h2>
 	</body> 
 </html>
