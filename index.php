844c48cfef492b8f34fc0b09b2a1d10a10844a51
sandervanvugt <mail@sandervanvugt.nl>
2022-10-18 18:44:35 +0200

    message

--- index.php
+++ index.php
@@ -11,6 +11,6 @@
 		echo date("g:i A l, F j Y.");?> </p>
 	<h2>PHP Information</h2> 
 		<p> <?php phpinfo(); ?> </p> 
-        <h2>GOING ON VACATION</h2>
+        <h2>NICE WEATHER IN NL TODAY</h2>
 	</body> 
 </html>
