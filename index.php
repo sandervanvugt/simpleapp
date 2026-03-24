43bff8df226370e45909e5c01d80e76632478326
sandervanvugt <mail@sandervanvugt.nl>
2026-03-24 09:50:28 +0100

    message

--- index.php
+++ index.php
@@ -11,6 +11,6 @@
 		echo date("g:i A l, F j Y.");?> </p>
 	<h2>PHP Information</h2> 
 		<p> <?php phpinfo(); ?> </p> 
-        <h2>LETS GO RUN in the soesterbergse bos</h2>
+        <h2>LETS GO CYCLE MONT VENTOUX</h2>
 	</body> 
 </html>
